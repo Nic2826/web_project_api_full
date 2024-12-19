919c9a97892ab45e7e72289e1031298c668cc349
Nic2826 <nicolalgarin2605@gmail.com>
2024-12-19 12:08:50 -0500

    Frontend has been removed

--- Frontend/src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-
-export default function Footer() {
-    return (
-        <footer className="footer">
-        <p className="footer__copyright">&copy; 2024 Nicolle Algarín</p>
-      </footer>
-    );
-}
